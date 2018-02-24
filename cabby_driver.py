0c7d44b2b07ac4ddf397829e8bc14943d37d2f73
Elizabeth Fregoso <emf65@case.edu>
2018-02-23 21:50:35 -0500

    Pull service list into the cache.  Still working on pulling pulses.

--- cabby_driver.py
+++ cabby_driver.py
@@ -2,23 +2,35 @@ from cabby import create_client
 import json
 import io
 
-def otx_get():
+# method for retrieving OTX pulses & placing them into the cache file
+# UNFINISHED - currently developing
 
+# API Key used for logging in -- BETA VERSION. still altering for custom credentials
+OTX_API_KEY = "de0e60ea625d2b840e464f5d44299fcd513a3da48d2d7dd8c3214474dc6dbadb"
+# OTX password -- currently set to "password"
+OTX_PASSWORD = "password"
+# buffer object for pulses
+pulses = []
 
 def main():
     # set up the OTX feed as a TAXII client
-    client = create_client('https://otx.alienvault.com', discovery_path='/taxii/discovery')
+    client = create_client('otx.alienvault.com', use_https=True, discovery_path='/taxii/discovery')
 
     # basic authentication -- password ignored in favor of full API key
-    client.set_auth(username='de0e60ea625d2b840e464f5d44299fcd513a3da48d2d7dd8c3214474dc6dbadb', password='password')
+    # IN PROGRESS
+    # client.set_auth(username=OTX_API_KEY, password=OTX_PASSWORD)
 
     # create a cache stream to a text file to test
     f = open("testcache.txt", "w")
 
-    # pull in a pulse & write it to the cache
-    # IN PROGRESS - currently only write simple Hello.
-    f.write("Hello")
+    # pull services from OTX
+    services = client.discover_services()
 
+    # write the service names to the cache
+    # IN PROGRESS - currently writes to cache the services available
+    for service in services:
+        f.write('Service type={s.type}, address={s.address}'.format(s=service))
+    # otx_get(f)
 
     # exits the program
     print("System done.")
