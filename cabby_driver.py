5c0be86c977cb13b64b95faa647d81930dd2b495
Elizabeth Fregoso <emf65@case.edu>
2018-03-02 14:20:04 -0500

    Change to OTXv2 worked. Currently prints the names of the indicators in all pulses that the user is subscribed to with the given API key.  Working on taking a user's API key & using that to import custom API data.

--- cabby_driver.py
+++ cabby_driver.py
@@ -1,5 +1,8 @@
 from OTXv2 import OTXv2
+import json
 from OTXv2 import IndicatorTypes
+from pandas.io.json import json_normalize
+from datetime import datetime, timedelta
 
 # method for retrieving OTX pulses & placing them into the cache file
 # IN PROGRESS - currently developing
@@ -8,18 +11,21 @@ from OTXv2 import IndicatorTypes
 otx = OTXv2("de0e60ea625d2b840e464f5d44299fcd513a3da48d2d7dd8c3214474dc6dbadb")
 # OTX password -- currently set to "password"
 OTX_PASSWORD = "password"
-# buffer object for pulses
-pulses = []
 # name of the cache file
-cache = 'stixtest.xml'
+cache = "testcache.txt"
 
 def main():
 
-    #get all indicators associated with a pulse
-    indicators = otx.get_pulse_indicators("")
-
-def findServices():
-
+    #open file stream to cache document
+    cachef = open(cache, "w")
+    #get all subscribed pulses
+    pulses = otx.getall()
+    #WIP: save to cache document
+    for indicator in pulses:
+        cachef.write(indicator["name"] + "\n")
+    #signal that the program is done
+    print("System done")
+    exit()
 
 
 if __name__ == '__main__':
