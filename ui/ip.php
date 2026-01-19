75d5d98b812dddeacece4c971a8b5864b2e55c1d
Elli <e.m.fregoso@proton.me>
2026-01-18 19:38:56 -0800

    * Cleanup HTML and PHP files, remove unused code, and standardize formatting * Rename "mockui" dir -> "ui"

---
+++ ui/ip.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>IP Lookup Service</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <!-- A navigation bar and a central view that loads.-->
+    <div id="top-nav-bar-container">
+        <nav id="top-nav">
+            <a href="index.html" id="site-title">OTX-V</a>
+            <a href="iplookup.html">IP Lookup</a>
+            <a href="portchecker.html">Port Checker</a>
+            <a href="stylo.html">STYLO</a>
+            <a href="http://localhost:32771/app/kibana">Kibana</a>
+            <a href="glossary.html">Glossary</a>
+        </nav>
+    </div>
+
+    <div id="main-body">
+        <h1>IP Address DNS Lookup</h1>
+        <form action="ip.php" method="post">
+            <label for="ip">IPv4 or IPv6 address</label>
+            <input type="text" name="ip" id="ip" required>
+            <input type="submit" value="submit" >
+        </form>
+    </div>
+
+    <?php
+        set_time_limit(100);
+
+        global $ip, $socket, $bytes, $address, $port, $ipinfo, $array, $bip;
+        $ip = $_POST["ip"];
+        $bip = base64_encode($ip);
+        $method = 'iplookup';
+        $bmethod = base64_encode($method);
+        $address = 'localhost';
+        $port = 10000;
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+        if ($socket === false){
+            die("socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n");
+        }
+
+        $result = socket_connect($socket, $address, $port);
+
+        if ($result === false) {
+            die("socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n");
+        };
+
+        socket_write($socket, $bmethod, strlen($bmethod));
+        sleep(2);
+        socket_write($socket, $bip, strlen($bip));
+        sleep(2);
+        
+        global $buf, $boolean, $ipinfo, $iparray;
+        $buf = socket_read($socket, 1024);
+        $ipinfo = (string)base64_decode($buf);
+        $iparray = preg_split('/~/', $ipinfo);
+    ?>
+
+    <table id="ip-info">
+        <tr>
+            <th>IP Address</th>
+            <td><?php echo $ip ?></td>
+        </tr>
+        <tr>
+            <th>ASN Description</th>
+            <td><?php echo $iparray[0] ?></td>
+        </tr>
+        <tr>
+            <th>IP Version</th>
+            <td><?php echo $iparray[1] ?></td>
+        </tr>
+        <tr>
+            <th>Owner Name</th>
+            <td><?php echo $iparray[2] ?></td>
+        </tr>
+        <tr>
+            <th>Owner Location</th>
+            <td><?php echo $iparray[3] ?></td>
+        </tr>
+        <tr>
+            <th>Contact Name</th>
+            <td><?php echo $iparray[4] ?></td>
+        </tr>
+        <tr>
+            <th>Contact Location</th>
+            <td><?php echo $iparray[5] ?></td>
+        </tr>
+        <tr>
+            <th>Contact Email</th>
+            <td><?php echo $iparray[6] ?></td>
+        </tr>
+        <tr>
+            <th>Contact Phone</th>
+            <td><?php echo $iparray[7] ?></td>
+        </tr>
+    </table>
+
+    <?php
+    socket_close($socket);
+    ?>
+</body>
+
+</html>
